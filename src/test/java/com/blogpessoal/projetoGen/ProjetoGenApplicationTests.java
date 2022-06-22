510ad6bd3364cd6e85714d0b1d09f5abd26b943d
JoaraGouveia <joarapaula2020@gmail.com>
2022-06-22 10:22:17 -0300

    blogpessoal

---
+++ src/test/java/com/blogpessoal/projetoGen/ProjetoGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogpessoal.projetoGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
